d29c3d4465a75ad84a9d1d27b9805e0bea433cee
João Carlos Matos <contaseassinaturasdojoao@gmail.com>
2021-10-20 21:27:32 -0300

    update para prod

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-from facebook_creds import  facebook_creds
 from base import metadata, csv_output, instagram_metadata_download
 import warnings
 
 warnings.filterwarnings("ignore")
 
 path_metadata = metadata()
-creds = facebook_creds()
-instagram_metadata_download(path_metadata, creds)
+instagram_metadata_download(path_metadata)
 csv_output(path_metadata)
