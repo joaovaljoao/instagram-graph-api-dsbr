746898ec78f53223515ff8faf743def2cceb63bd
João Carlos <joao.oliveira@ufob.edu.br>
2022-04-19 22:54:29 -0300

    Novo main, agora vamos usá-lo para parsear input de dados

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-from base import metadata, csv_output, instagram_metadata_download
-import warnings
+from instagram_feed import InstagramGraphApi
+import json
 
-warnings.filterwarnings("ignore")
+# download instagram data
+username = 'cnpq_oficial', 'fapergs', 'finepinova', 'capes_oficial', 'agenciafapesp', 'fapesb'
 
-path_metadata = metadata()
-instagram_metadata_download(path_metadata)
-csv_output(path_metadata)
+# bd = InstagramGraphApi().business_discovery(i, pages=1)
+top_media = InstagramGraphApi().top_media('ufob', pages=1)
+filter = InstagramGraphApi().filter_caption('PROCESSO SELETIVO', top_media)
+
+#write json
+
+with open('top_media.json', 'w') as outfile:
+    json.dump(filter, outfile, indent=4)
