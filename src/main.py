7d0d4efa9a6805d46f701ddb0270e575d26373a6
João Carlos <joao.oliveira@ufob.edu.br>
2022-12-22 20:22:24 -0300

    Adicionan Resizer de Imagens

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from business_data import BusinessData
 from user_data import UserData
 from image_downloader import InstagramImageDownloader
+from image_resizer import ImageResizer
 
 # Create the UserData instance
 user_data = UserData('usernames/ifes.csv')
@@ -10,12 +11,13 @@ usernames = user_data.get_usernames()
 FIELDS = "{username,website,name,ig_id,id,profile_picture_url,biography,\
         follows_count,followers_count,media_count,media{media_url,comments_count,\
         like_count,caption,media_type,permalink,timestamp,username}}"
-
+    
 # Create the BusinessData instance
 business_data = BusinessData()
 
 # Create an InstagramImageDownloader instance
 downloader = InstagramImageDownloader()
+
 
 # Iterate over the list of usernames
 for username in usernames[:2]: # Limitado a 2 para não sobrecarregar a API, Mudar na produção
@@ -24,8 +26,7 @@ for username in usernames[:2]: # Limitado a 2 para não sobrecarregar a API, Mud
     # Save the data to CSV files
     business_data.save_to_csv(response, username)
     downloader.download_images(response)
-
-
-
-print('fim')
-
+    resizer = ImageResizer('ifes_media.csv')
+    resizer.process_directory('images')
+    # save the ImageResizer object to a pickle file
+    resizer.save('resizer.pkl')
