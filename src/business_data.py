8122a2fca826dd28c08444ae5acf14b92312b67c
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2022-12-29 23:21:06 -0300

    business_data to funcion, fix .get in dict and loggin time and more info

--- src/business_data.py
+++ src/business_data.py
@@ -9,58 +9,61 @@ import dotenv
 # Load the environment variables
 dotenv.load_dotenv()
 
-class BusinessData:
-    def __init__(self):
-        self.input_token = os.getenv('LONG_LIVED_TOKEN')
-        self.endpoint = os.getenv('GRAPH_DOMAIN') + os.getenv('VERSION') + "/" + os.getenv('FACEBOOK_ID')
-        loggin_setup()
-        create_folder()
+input_token = os.getenv('LONG_LIVED_TOKEN')
+endpoint = os.getenv('GRAPH_DOMAIN') + os.getenv('VERSION') + "/" + os.getenv('FACEBOOK_ID')
 
-    def get_business_data(self, username: str, fields: str) -> dict:
-        params = {
-            'fields': f'business_discovery.username({username})' + fields,
-            'access_token': self.input_token,
-        }
-        timestamp = time.time()
+def get_business_data(username: str, fields: str) -> dict:
+    '''
+    Faz um request ao Graph API para obter os dados de um usuário do Instagram em formato de dicionário.
+    
+    Args:
+        username: String com o nome de usuário do Instagram.
+        fields: String com os campos que devem ser retornados pelo request.
+    '''
+    params = {
+        'fields': f'business_discovery.username({username})' + fields,
+        'access_token': input_token,
+    }
+    timestamp = time.time()
 
-        try:
-            # Make the GET request
-            response = requests.get(self.endpoint, params=params)
+    try:
+        # Make the GET request
+        response = requests.get(endpoint, params=params)
 
-            # Check the status code
-            if response.status_code != 200:
-                logging.warning(f'{timestamp} - {username} - Error: {response.status_code}')
-                with open(f'logging/pickle/{timestamp}_business_data.pickle', 'wb') as handle:
-                    pickle.dump(response, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        # Check the status code
+        if response.status_code != 200:
+            logging.warning(f'{timestamp} - {username} - Error: {response.status_code}')
+            with open(f'logging/pickle/{timestamp}_business_data.pickle', 'wb') as handle:
+                pickle.dump(response, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        except requests.ConnectionError as e:
-            logging.error(f'ConnectionError: {e}')
-            return None
-        except requests.HTTPError as e:
-            logging.error(f'HTTPError: {e}')
-            return None
-        except Exception as e:
-            logging.error(f'Error: {e}')
-            return None
-        
-        return response.json()
+    except requests.ConnectionError as e:
+        logging.error(f'ConnectionError: {e}')
+        return None
+    except requests.HTTPError as e:
+        logging.error(f'HTTPError: {e}')
+        return None
+    except Exception as e:
+        logging.error(f'Error: {e}')
+        return None
+    
+    return response.json()
 
 
-    def save_to_csv(self, data: dict, filename_prefix: str) -> None:
-        # Extract the data for the user and the media
-        user_data = {k: v for k, v in data['business_discovery'].items() if k != 'media'}
-        media_data = [{**{'id': data['business_discovery']['id']}, **item} for item in data['business_discovery']['media']['data']]
+def save_to_csv(data: dict, filename_prefix: str) -> None:
+    # Extract the data for the user and the media
+    user_data = {k: v for k, v in data['business_discovery'].items() if k != 'media'}
+    media_data = [{**{'id': data['business_discovery']['id']}, **item} for item in data['business_discovery']['media']['data']]
 
-        # Create the pandas DataFrames
-        df_user = pd.DataFrame(user_data, index=[0])
-        df_media = pd.DataFrame(media_data)
+    # Create the pandas DataFrames
+    df_user = pd.DataFrame(user_data, index=[0])
+    df_media = pd.DataFrame(media_data)
 
-        # # Create the images folder if it doesn't exist
-        # create_folder()
+    # # Create the images folder if it doesn't exist
+    # create_folder()
 
-        # Save the DataFrames to CSV files
-        df_user.to_csv('pub/'+filename_prefix+'_user_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_user_data.csv'))
-        df_media.to_csv('pub/'+filename_prefix+'_media_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_media_data.csv'))
+    # Save the DataFrames to CSV files
+    df_user.to_csv('pub/'+filename_prefix+'_user_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_user_data.csv'))
+    df_media.to_csv('pub/'+filename_prefix+'_media_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_media_data.csv'))
 
 def loggin_setup():
     # create logging folder if it doesn't exist
@@ -72,7 +75,11 @@ def loggin_setup():
         os.makedirs('logging/pickle')
 
     # Configure logging
-    logging.basicConfig(filename='logging/app.log', filemode='w', format='%(levelname)s - %(message)s', level=logging.DEBUG)
+    logging.basicConfig(filename='logging/app.log', 
+                        filemode='a', 
+                        format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s', 
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=logging.DEBUG)
 
 def create_folder():
     '''create folder if it doesn't exist'''
