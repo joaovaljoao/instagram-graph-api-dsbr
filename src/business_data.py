5a72860fe7005b8d277f95537e8320221f16f2ec
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2022-12-29 23:47:19 -0300

    flake8 ok com --max-line-length 110

--- src/business_data.py
+++ src/business_data.py
@@ -10,12 +10,15 @@ import dotenv
 dotenv.load_dotenv()
 
 input_token = os.getenv('LONG_LIVED_TOKEN')
-endpoint = os.getenv('GRAPH_DOMAIN') + os.getenv('VERSION') + "/" + os.getenv('FACEBOOK_ID')
+endpoint = os.getenv('GRAPH_DOMAIN') + os.getenv('VERSION') + \
+           "/" + os.getenv('FACEBOOK_ID')
+
 
 def get_business_data(username: str, fields: str) -> dict:
     '''
-    Faz um request ao Graph API para obter os dados de um usuário do Instagram em formato de dicionário.
-    
+    Faz um request ao Graph API para obter os dados de um usuário do
+    Instagram em formato de dicionário.
+
     Args:
         username: String com o nome de usuário do Instagram.
         fields: String com os campos que devem ser retornados pelo request.
@@ -27,15 +30,11 @@ def get_business_data(username: str, fields: str) -> dict:
     timestamp = time.time()
 
     try:
-        # Make the GET request
         response = requests.get(endpoint, params=params)
-
-        # Check the status code
         if response.status_code != 200:
             logging.warning(f'{timestamp} - {username} - Error: {response.status_code}')
             with open(f'logging/pickle/{timestamp}_business_data.pickle', 'wb') as handle:
                 pickle.dump(response, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
     except requests.ConnectionError as e:
         logging.error(f'ConnectionError: {e}')
         return None
@@ -45,14 +44,21 @@ def get_business_data(username: str, fields: str) -> dict:
     except Exception as e:
         logging.error(f'Error: {e}')
         return None
-    
     return response.json()
 
 
 def save_to_csv(data: dict, filename_prefix: str) -> None:
+    '''
+    Salva os dados de um usuário do Instagram em um arquivo CSV.
+
+    Args:
+        data: Dicionário com os dados de um usuário do Instagram.
+        filename_prefix: String com o prefixo do nome do arquivo CSV.
+    '''
     # Extract the data for the user and the media
     user_data = {k: v for k, v in data['business_discovery'].items() if k != 'media'}
-    media_data = [{**{'id': data['business_discovery']['id']}, **item} for item in data['business_discovery']['media']['data']]
+    media_data = [{**{'id': data['business_discovery']['id']},
+                   **item} for item in data['business_discovery']['media']['data']]
 
     # Create the pandas DataFrames
     df_user = pd.DataFrame(user_data, index=[0])
@@ -62,24 +68,30 @@ def save_to_csv(data: dict, filename_prefix: str) -> None:
     # create_folder()
 
     # Save the DataFrames to CSV files
-    df_user.to_csv('pub/'+filename_prefix+'_user_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_user_data.csv'))
-    df_media.to_csv('pub/'+filename_prefix+'_media_data.csv', index=False, sep=';', encoding='utf-8-sig', mode='a', header=not os.path.exists('pub/'+filename_prefix+'_media_data.csv'))
+    df_user.to_csv('pub/'+filename_prefix+'_user_data.csv',
+                   index=False, sep=';', encoding='utf-8-sig',
+                   mode='a',
+                   header=not os.path.exists('pub/'+filename_prefix+'_user_data.csv'))
+    df_media.to_csv('pub/'+filename_prefix+'_media_data.csv',
+                    index=False, sep=';',
+                    encoding='utf-8-sig', mode='a',
+                    header=not os.path.exists('pub/'+filename_prefix+'_media_data.csv'))
+
 
 def loggin_setup():
+    '''setup logging'''
     # create logging folder if it doesn't exist
     if not os.path.exists('logging'):
         os.makedirs('logging')
-
     # create pickle folder if it doesn't exist in the logging directory
     if not os.path.exists('logging/pickle'):
         os.makedirs('logging/pickle')
-
-    # Configure logging
-    logging.basicConfig(filename='logging/app.log', 
-                        filemode='a', 
-                        format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s', 
+    logging.basicConfig(filename='logging/app.log',
+                        filemode='a',
+                        format='%(asctime)s - %(levelname)s - %(module)s - %(funcName)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
                         level=logging.DEBUG)
+
 
 def create_folder():
     '''create folder if it doesn't exist'''
